4aa6770849dd9837c0be9903453a3cc3c38922ae
Rodrigo Soares <adsrodsoares@gmail.com>
2022-05-17 17:58:53 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my site");
+})
+
+app.listen(8080, () => {
+    console.log("Server running!")
+})
